6172e618248f82e42ea5ae97e04f79267fdfa012
300083357 <300083357@fvtc.edu>
2024-03-19 16:01:29 -0500

    added Product model

---
+++ DemoWebApi/DemoApi/Data/DemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DemoApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoApi.Data
+{
+    public class DemoDbContext: DbContext
+    {
+        public DemoDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { 
+            
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DemoWebApi/DemoApi/Domain/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoApi.Domain.Entities
+{
+    //A record in C# is a class or struct that provides special syntax and behavior for working with data models.
+
+    [Table("product")]
+    public record Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; init; }
+
+        [Column("product_name")]
+        [MaxLength(100)]
+        public string? ProductName { get; init; }
+
+
+        [Column("product_code")]
+        [MaxLength(10)]
+        public string? ProductCode { get; init;}
+
+        [Column("price", TypeName = "decima(7, 2)")]
+        [MaxLength(10)]
+        public decimal Price { get; init; }
+    }
+}
